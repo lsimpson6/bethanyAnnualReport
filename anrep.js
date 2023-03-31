4b46548b303386ac98fb7213fa7675d8d8057832
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-03-31 14:14:46 -0400

    SwitchText_1

--- anrep.js
+++ anrep.js
@@ -3,11 +3,22 @@
   var textSelector = ["Annual Report", "Motivated by Faith", "Global Impact", "Mission and Vision"];
   var target = document.querySelectorAll('.title-target');
   var tLength = target.length;
+
+  (function (){
+    sectionTitle.textContent = textSelector[0];
+  })();
   
     window.addEventListener('scroll', ()=>{
 
-      var y = scrollY;
-      alert(y);
+      for(var i = 0; i < target.length; i++){
+        var t = target[i].getClientBoundingRect().top;
+        var x = 0;
+        if(t <= 0){
+          x++;
+          sectionTitle.textContent = textSelector[x];
+        }
+      }
+
     });
       
 })();
