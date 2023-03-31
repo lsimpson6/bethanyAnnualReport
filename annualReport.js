7fe38452b61acd445c238737eff960dff0ce87fa
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-03-31 13:44:23 -0400

    Delete annualReport.js

--- annualReport.js
+++
@@ -1,9 +0,0 @@
-(function (){
-  var sectionTitle = document.getElementById('section-title').innerHTML;
-  
-  window.addEventListener('scroll', ()=>{
-  	var y = scrollY;
-    
-      alert(y);
-  })
-})();
