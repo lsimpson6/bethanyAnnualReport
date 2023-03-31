053f15ce42763a0dc55857ba97dd41b5ea2010da
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-03-31 14:24:06 -0400

    test-1

--- anrep.js
+++ anrep.js
@@ -10,15 +10,28 @@
   
     window.addEventListener('scroll', ()=>{
 
-      for(var i = 0; i < target.length; i++){
+      /*for(var i = 0; i < target.length; i++){
         var t = target[i].getClientBoundingRect().top;
         var x = 0;
         if(t <= 0){
           x++;
-          sectionTitle.textContent = textSelector[x];
         }
+        sectionTitle.textContent = textSelector[x];
+      }*/
+      var x = 0;
+
+      if(scrollY > 30){
+        x = 1;
+      }
+      else if(scrollY > 60){
+        x = 2;
+      }
+      else {
+        x = 3;
       }
 
+
+        sectionTitle.textContent = textSelector[x];
     });
       
 })();
