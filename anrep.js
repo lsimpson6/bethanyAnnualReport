f1629d7dc9ec24f82b3f0d2134a2d3f395ce6015
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-03-31 14:09:20 -0400

    Fix

--- anrep.js
+++ anrep.js
@@ -1,18 +1,17 @@
-<script>
-  
 (function (){
   var sectionTitle = document.getElementById('section-title');
   var textSelector = ["Annual Report", "Motivated by Faith", "Global Impact", "Mission and Vision"];
   var target = document.querySelectorAll('.title-target');
   var tLength = target.length;
   
-window.addEventListener('scroll', ()=>{
-  	var y = scrollY;
-    alert(y);
-  })
+window.addEventListener('scroll', animate);
+
+function animate(){
+  var y = scrollY;
+  alert(y);
+};
   
   
   
 })();
 
-</script>
