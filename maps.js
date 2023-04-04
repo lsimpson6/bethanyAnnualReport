d2f5ffdecf6c8f386ae6e27213c3b7b70b37af9d
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-04-04 13:23:15 -0400

    added country data

--- maps.js
+++ maps.js
@@ -1,17 +1,3 @@
-var unitedStatesMap = document.getElementById('united-states-map');
-var unitedStatesPin = document.getElementById('united-states-pin');
-var romaniaMap = document.getElementById('Romania-Map');
-var romaniaPin = document.getElementById('Romania-Pin');
-var ghanaMap = document.getElementById('Ghana-Map');
-var ghanaPin = document.getElementById('Ghana-Pin');
-var colombiaMap = document.getElementById('Colombia-Map');
-var colombiaPin = document.getElementById('Colombia-Pin');
-var haitiMap = document.getElementById('Haiti-Map');
-var haitiPin = document.getElementById('Haiti-Pin');
-var albaniaMap = document.getElementById('Albania-Map');
-var albaniaPin = document.getElementById('Albania-Pin');
-var ethipioaMap = document.getElementById('Ethipoia-Map');
-var ethipioaPin = document.getElementById('Ethiopia-Pin');
 
 var countryName = document.getElementById('country-Name-wrldmp');
 var numServed = document.getElementById('stat-one-wrldmp');
@@ -19,21 +5,26 @@ var numPrograms = document.getElementById('stat-two-wrldmp');
 var numLocations = document.getElementById('stat-three-wrldmp');
 var numExpenditures = document.getElementById('stat-four-wrldmp');
 
-(function(){
-  switchLocationStats(0);
-})();
+const states = [
+{state: "Arkansas", served: "432", programs: "4", locations: "1", expenditures: "$218,682"}
+];
 
-function switchLocationStats(name, served, programs, locations, expenditures){
-  countryText.textContent = name;
+const countries = [
+    {country: "United States", served: "100,000", programs: "280", locations: "35", expenditures: "$218,682"},
+    {country: "Colombia", served: "12,496", programs: "1", locations: "3", expenditures: "$472,665"},
+    {country: "Ghana", served: "309", programs: "4", locations: "1", expenditures: "$370,726"},
+    {country: "Ethiopia", served: "9,056", programs: "6", locations: "5", expenditures: "$1,777,025"},
+    {country: "Haiti", served: "36", programs: "2", locations: "1", expenditures: "$307,886"},
+    {country: "South Africa", served: "4,267", programs: "3", locations: "1", expenditures: "$247,577"},
+    {country: "Albania", served: "189", programs: "3", locations: "1", expenditures: "$80,865"},
+    {country: "Romania", served: "825", programs: "1", locations: "1", expenditures: "$108,039"}
+];
+
+function globalStats(index){
+  var countries = x;
+  countryName.textContent =  x.country(index);
   numServed.textContent = served;
-  numPrograms.textContent = programs
-  numLocations.textContent = locations
-  numExpenditures.textContent = expenditures
+  numPrograms.textContent = programs;
+  numLocations.textContent = locations;
+  numExpenditures.textContent = expenditures;
 }
-
-/*let USDollar = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-});
-
-console.log(USDollar.format(price));*/
