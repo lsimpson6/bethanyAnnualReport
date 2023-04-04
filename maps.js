55c313c91635a0f3a6ae27a304fe38c91c8afb11
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-04-04 13:43:53 -0400

    updated country selector

--- maps.js
+++ maps.js
@@ -20,11 +20,23 @@ const countries = [
     {country: "Romania", served: "825", programs: "1", locations: "1", expenditures: "$108,039"}
 ];
 
+/*const countries = {
+  "gl-us": {country: "United States", served: "100,000", programs: "280", locations: "35", expenditures: "$218,682"},
+  "gl-cl": {country: "Colombia", served: "12,496", programs: "1", locations: "3", expenditures: "$472,665"},
+  "gl-gh": {country: "Ghana", served: "309", programs: "4", locations: "1", expenditures: "$370,726"},
+  "gl-et": {country: "Ethiopia", served: "9,056", programs: "6", locations: "5", expenditures: "$1,777,025"},
+  "gl-ht": {country: "Haiti", served: "36", programs: "2", locations: "1", expenditures: "$307,886"},
+  "gl-sa": {country: "South Africa", served: "4,267", programs: "3", locations: "1", expenditures: "$247,577"},
+  "gl-al": {country: "Albania", served: "189", programs: "3", locations: "1", expenditures: "$80,865"},
+  "gl-rm": {country: "Romania", served: "825", programs: "1", locations: "1", expenditures: "$108,039"}
+};
+*/
+
 function globalStats(index){
-  var countries = x;
-  countryName.textContent =  x.country(index);
-  numServed.textContent = served;
-  numPrograms.textContent = programs;
-  numLocations.textContent = locations;
-  numExpenditures.textContent = expenditures;
+  var countries = c;
+  countryName.textContent = c[index].country;
+  numServed.textContent = c[index].served;
+  numPrograms.textContent = c[index].programs;
+  numLocations.textContent = c[index].locations;
+  numExpenditures.textContent = c[index].expenditures;
 }
