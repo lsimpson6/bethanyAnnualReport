7061fb2e875dafab5d32e3ee3d173f8f925c29d9
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-04-17 11:00:41 -0400

    remove desktop only

--- anrep.js
+++ anrep.js
@@ -17,11 +17,6 @@
   var tmpHeight = 0;
   if(window.innerWidth < 768){
     tmpHeight = winHeight/1.5;
-    for(var deo = 0; deo < desktopOnly.length; deo ++){
-      if($(desktopOnly[deo]).hasClass('title-target')){
-        desktopOnly[deo].classList.remove('title-target');
-      }
-    }
   }
   else {
     tmpHeight = winHeight/1.5;
