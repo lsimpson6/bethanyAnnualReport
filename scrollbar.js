ed6f4316befbafbfb238b0b9f95d25b594a3bf81
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-26 10:32:48 -0400

    added screen height

--- scrollbar.js
+++ scrollbar.js
@@ -2,12 +2,13 @@ var scrollBar = document.getElementById('site-progress');
 var body = document.body, html = document.documentElement;
 var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 var w = window.innerWidth;
-var incr = h/w;
+var screenHeight = window.screenHeight;
 var tmpWidthHeight = 0;
 var oldValue = 0;
 
 window.addEventListener('scroll', ()=>{
     
+    var incr = (h-screenHeight)/w;
     newValue = window.pageYOffset;
 
     if(oldValue - newValue < 0){
