834f0ec49ba1e9e82acfbf34b6a4c5b89715ab4c
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-26 10:06:35 -0400

    added body

--- scrollbar.js
+++ scrollbar.js
@@ -1,4 +1,5 @@
 var scrollBar = document.getElementById('site-progress');
+var body = document.body, html = document.documentElement;
 var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 var w = window.innerWidth;
 var scroll = scrollY;
