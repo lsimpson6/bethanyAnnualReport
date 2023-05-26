63eeeaf5515fc41a01ced2dc58d40bb49db556de
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-26 10:29:00 -0400

    added incr

--- scrollbar.js
+++ scrollbar.js
@@ -2,19 +2,18 @@ var scrollBar = document.getElementById('site-progress');
 var body = document.body, html = document.documentElement;
 var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 var w = window.innerWidth;
-var widthOverHeight = w/h;
+var incr = h/w;
 var tmpWidthHeight = 0;
 var oldValue = 0;
 
 window.addEventListener('scroll', ()=>{
     
     newValue = window.pageYOffset;
-    tmpWidthHeight += widthOverHeight;
 
     if(oldValue - newValue < 0){
-        tmpWidthHeight -= widthOverHeight;
+        tmpWidthHeight += incr;
     } else if(oldValue - newValue > 0){
-        tmpWidthHeight += widthOverHeight;
+        tmpWidthHeight -= incr;
     }
 
     oldValue = newValue;
