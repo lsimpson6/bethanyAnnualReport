dbd96a34675e5d965994a6cc148c295fe78754c3
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-26 10:17:34 -0400

    Update scrollbar.js

--- scrollbar.js
+++ scrollbar.js
@@ -2,11 +2,10 @@ var scrollBar = document.getElementById('site-progress');
 var body = document.body, html = document.documentElement;
 var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 var w = window.innerWidth;
-var scroll = scrollY;
 var widthOverHeight = w/h;
+var tmpWidthHeight = 0;
 
 window.addEventListener('scroll', ()=>{
-    let tmpWidthHeight = 0;
     tmpWidthHeight += widthOverHeight;
     scrollBar.style.width = `${tmpWidthHeight}px`;
 })
