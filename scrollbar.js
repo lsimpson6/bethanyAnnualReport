09a8b5426f4690cbd1c4e5d7082f5a3d2ddbae8e
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-05-30 09:53:04 -0400

    new scroll function

--- scrollbar.js
+++ scrollbar.js
@@ -1,24 +1,15 @@
 var scrollBar = document.getElementById('site-progress');
-var body = document.body, html = document.documentElement;
-var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-var w = window.innerWidth;
-var screenHeight = screen.clientHeight;
-var tmpWidthHeight = 0;
-var oldValue = 0;
-var onePercent = h*.01;
 
-
-var amountScrolled = scrollY;
-var barWidth = 0;
-window.addEventListener('scroll', ()=>{
-
-    console.log("percent: " + onePercent);
-
-    if(scrollY % onePercent === 0){
-        barWidth ++;
-        scrollBar.style.width = `${barWidth}%`;
-        console.log("scrolled: " + scrollY);
-    }
-  
-
-})
+document.addEventListener(
+    "scroll",
+    function() {
+      var scrollTop =
+        document.documentElement["scrollTop"] || document.body["scrollTop"];
+      var scrollBottom =
+        (document.documentElement["scrollHeight"] ||
+          document.body["scrollHeight"]) - document.documentElement.clientHeight;
+      scrollPercent = scrollTop / scrollBottom * 100 + "%";
+      scrollBar.style.width = `${scrollPercent}%`;
+    },
+    { passive: true }
+  );
